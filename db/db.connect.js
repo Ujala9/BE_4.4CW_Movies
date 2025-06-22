0982684289be6efe148143b4213a5ba771e9c145
techei <ujalabharti7816@gmail.com>
2025-06-22 13:38:16 +0530

    fix: fixing db.connect

--- db/db.connect.js
+++ db/db.connect.js
@@ -4,15 +4,17 @@ require("dotenv").config();
 const mongoURI = process.env.MONGODB_URI
 
 const initializeDatabase = async  () => {
-   try {
-    const connection = await mongoose.connect(mongoURI)
+  try {
+    const connection = await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     if (connection) {
-        console.log('Connected Successfully')
-     }
-    } catch (error) {
-        console.log('Connection Failed', error)
-    
+      console.log("connected succesfully");
     }
+  } catch (error) {
+    console.log("connection failed", error);
+  }
 }
 
 module.exports = { initializeDatabase }
